002c9b2c9dbd177ad24c649b58c24320baeaa00d
Jane <jane42242002@gmail.com>
2021-09-18 04:34:54 +0800

    feat: add some dependencies untrack dist/ folder

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 
 const main = async () => {
-    const orm = await MikroORM.init(microConfig);
-    console.log(orm.em);
-    await orm.getMigrator().up();
-    const post = orm.em.create(Post, { title: "first post" });
-    await orm.em.persistAndFlush(post);
-    console.log("??");
+  const orm = await MikroORM.init(microConfig);
+  // console.log(orm.em);
+  await orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: "first post" });
+  // await orm.em.persistAndFlush(post);
 
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
+
+  const app = express();
+  app.get("/", (/*req*/ _, res) => {
+    res.send("hello");
+  });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((error) => {
-    console.error(error);
+  console.error(error);
 });
-
-console.log("aa");
