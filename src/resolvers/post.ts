bdf332b46c8f3b2d110c359ea65b2a18ec7cc501
Jane <jane42242002@gmail.com>
2021-09-29 02:17:56 +0800

    feat: createPost() and updatePost() query in PostResolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,5 @@
 import { MyContext } from "src/types";
-import { Arg, Ctx, Query, Int } from "type-graphql";
+import { Arg, Ctx, Query, Int, Mutation } from "type-graphql";
 import { Post } from "../entities/Post";
 
 export class PostResolver {
@@ -14,5 +14,30 @@ export class PostResolver {
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: String,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post)
+  async updatePost(
+    @Arg("id", () => Int) id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) return null;
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
   }
 }
